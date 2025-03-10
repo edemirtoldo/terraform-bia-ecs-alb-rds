67401bac8b4c95febcd62456637981cbf5e9ce12
Edemir Toldo <edemirtoldo@gmail.com>
2025-03-10 14:38:19 -0300

    Alterado as definições de variáveis locais

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-    vpc_id="vpc-0190d942f4fdb18a7"
-    subnet_zona_a="subnet-0345d28f0241f9a22"
-    subnet_zona_b="subnet-0eb72aafcbc66dccf"
+  vpc_id="[SEU_VPC_IP]"
+  subnet_zona_a="[SUA_SUBNET_ZONA_A]"
+  subnet_zona_b="[SUA_SUBNET_ZONA_B]"
 }
